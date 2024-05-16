ffee9ed19b38e1a77966ded74954fc021a7d870a
drewvex23 <drewveilleux24@gmail.com>
2024-05-16 12:42:18 -0400

    commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const burgerMenu = document.querySelector('.burger-menu')
 const mobileLinks = document.querySelector('.mobile-links')
 const openBurgerMenu = document.querySelector('.open-burger-menu')
+const galleryButton = document.querySelector('.gallery-button')
+const leftArrow = document.querySelector('.left-arrow-gallery')
+const rightArrow = document.querySelector('.right-arrow-gallery')
+const galleryDiv = document.querySelector('.gallery-div')
+
+
+const galleryImagesPreview = ['./images/image1.webp','./images/image2.jpg','./images/image3.jpg','./images/image4.jpg','./images/image5.avif','./images/image6.jpg']
+
 
 burgerMenu.addEventListener('click', () => {
     mobileLinks.style.display = 'flex'
@@ -12,4 +20,13 @@ openBurgerMenu.addEventListener('click', () => {
     mobileLinks.style.display = 'none'
     burgerMenu.style.display = 'block'
     openBurgerMenu.style.display = 'none'
+})
+
+galleryButton.addEventListener('click', () => {
+    location.href = './HTML/gallery.html'
+})
+
+rightArrow.addEventListener('click', () => {
+    
+    galleryDiv.style.backgroundImage = "url('./images/image1.webp')" 
 })
